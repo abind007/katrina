83eb3286497555921d2c1b0ed42e81b6c55fa5ba
abind007 <83115884+abind007@users.noreply.github.com>
2022-01-28 22:01:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -724,10 +724,8 @@ async def auto_filter(client, msg, spoll=False):
         await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     if spoll:
         await msg.message.delete()
-        await
-        asyncio.sleep(60)
-        await
-        message.delete()
+        await asyncio.sleep(60)
+        await message.delete()
 
 
 async def advantage_spell_chok(msg):
